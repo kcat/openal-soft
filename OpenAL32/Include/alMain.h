34a0c998cd849ab40a1e6325b37ca53c2740c355
Chris Robinson <chris.kcat@gmail.com>
2010-07-18 06:21:16 -0700

    Use CLOCK_MONOTONIC when available

--- OpenAL32/Include/alMain.h
+++ OpenAL32/Include/alMain.h
@@ -184,16 +184,26 @@ static __inline void DeleteCriticalSection(CRITICAL_SECTION *cs)
  * as opposed to the actual time. */
 static __inline ALuint timeGetTime(void)
 {
-    int ret;
 #if _POSIX_TIMERS > 0
+    static int hasmono = 0;
     struct timespec ts;
+    int ret = -1;
 
-    ret = clock_gettime(CLOCK_REALTIME, &ts);
+#if defined(_POSIX_MONOTONIC_CLOCK) && (_POSIX_MONOTONIC_CLOCK >= 0)
+#if _POSIX_MONOTONIC_CLOCK == 0
+    if(hasmono > 0 || (hasmono == 0 &&
+                       (hasmono=sysconf(_SC_MONOTONIC_CLOCK)) > 0))
+#endif
+        ret = clock_gettime(CLOCK_MONOTONIC, &ts);
+#endif
+    if(ret != 0)
+        ret = clock_gettime(CLOCK_REALTIME, &ts);
     assert(ret == 0);
 
     return ts.tv_nsec/1000000 + ts.tv_sec*1000;
 #else
     struct timeval tv;
+    int ret;
 
     ret = gettimeofday(&tv, NULL);
     assert(ret == 0);

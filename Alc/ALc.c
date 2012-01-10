e772fb7d0e1559915944de032c5fd0cb1370a8cf
Chris Robinson <chris.kcat@gmail.com>
2012-01-10 01:14:02 -0800

    Finalize AL_SOFT_buffer_samples

--- Alc/ALc.c
+++ Alc/ALc.c
@@ -376,7 +376,7 @@ static const ALchar alExtList[] =
     "AL_EXT_ALAW AL_EXT_DOUBLE AL_EXT_EXPONENT_DISTANCE AL_EXT_FLOAT32 "
     "AL_EXT_IMA4 AL_EXT_LINEAR_DISTANCE AL_EXT_MCFORMATS AL_EXT_MULAW "
     "AL_EXT_MULAW_MCFORMATS AL_EXT_OFFSET AL_EXT_source_distance_model "
-    "AL_LOKI_quadriphonic AL_SOFTX_buffer_samples AL_SOFT_buffer_sub_data "
+    "AL_LOKI_quadriphonic AL_SOFT_buffer_samples AL_SOFT_buffer_sub_data "
     "AL_SOFTX_deferred_updates AL_SOFT_loop_points "
     "AL_SOFTX_non_virtual_channels";
 

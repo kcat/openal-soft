a98e381c8e1406ec36a4f774fa418c3c17a1fd78
Chris Robinson <chris.kcat@gmail.com>
2012-02-07 08:05:01 -0800

    Ensure ALbyte3 and ALubyte3 are the proper size

--- OpenAL32/alBuffer.c
+++ OpenAL32/alBuffer.c
@@ -1068,9 +1068,11 @@ typedef ALubyte ALima4;
 typedef struct {
     ALbyte b[3];
 } ALbyte3;
+extern ALbyte ALbyte3_size_is_not_3[(sizeof(ALbyte3)==sizeof(ALbyte[3]))?1:-1];
 typedef struct {
     ALubyte b[3];
 } ALubyte3;
+extern ALbyte ALubyte3_size_is_not_3[(sizeof(ALubyte3)==sizeof(ALubyte[3]))?1:-1];
 
 static __inline ALshort DecodeMuLaw(ALmulaw val)
 { return muLawDecompressionTable[val]; }

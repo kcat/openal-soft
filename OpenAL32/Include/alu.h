c802bfccef732cac9e9cd31185e759eeb1f521f6
Chris Robinson <chris.kcat@gmail.com>
2012-10-25 14:16:35 -0700

    Remove the float math wrapper functions

--- OpenAL32/Include/alu.h
+++ OpenAL32/Include/alu.h
@@ -21,61 +21,6 @@
 #endif
 
 
-#ifndef HAVE_POWF
-static __inline float powf(float x, float y)
-{ return (float)pow(x, y); }
-#endif
-
-#ifndef HAVE_SQRTF
-static __inline float sqrtf(float x)
-{ return (float)sqrt(x); }
-#endif
-
-#ifndef HAVE_COSF
-static __inline float cosf(float x)
-{ return (float)cos(x); }
-#endif
-
-#ifndef HAVE_SINF
-static __inline float sinf(float x)
-{ return (float)sin(x); }
-#endif
-
-#ifndef HAVE_ACOSF
-static __inline float acosf(float x)
-{ return (float)acos(x); }
-#endif
-
-#ifndef HAVE_ASINF
-static __inline float asinf(float x)
-{ return (float)asin(x); }
-#endif
-
-#ifndef HAVE_ATANF
-static __inline float atanf(float x)
-{ return (float)atan(x); }
-#endif
-
-#ifndef HAVE_ATAN2F
-static __inline float atan2f(float x, float y)
-{ return (float)atan2(x, y); }
-#endif
-
-#ifndef HAVE_FABSF
-static __inline float fabsf(float x)
-{ return (float)fabs(x); }
-#endif
-
-#ifndef HAVE_LOG10F
-static __inline float log10f(float x)
-{ return (float)log10(x); }
-#endif
-
-#ifndef HAVE_FLOORF
-static __inline float floorf(float x)
-{ return (float)floor(x); }
-#endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif

8a00c240126d4138df21630f630da4c4f551000d
Chris Robinson <chris.kcat@gmail.com>
2014-04-17 09:22:57 -0700

    Fix SetThreadName for 64-bit MSVC builds

--- Alc/threads.c
+++ Alc/threads.c
@@ -54,19 +54,21 @@ void SetThreadName(const char *name)
 {
 #if defined(_MSC_VER)
 #define MS_VC_EXCEPTION 0x406D1388
+#pragma pack(push,8)
     struct {
         DWORD dwType;     // Must be 0x1000.
         LPCSTR szName;    // Pointer to name (in user addr space).
         DWORD dwThreadID; // Thread ID (-1=caller thread).
         DWORD dwFlags;    // Reserved for future use, must be zero.
     } info;
+#pragma pack(pop)
     info.dwType = 0x1000;
     info.szName = name;
     info.dwThreadID = -1;
     info.dwFlags = 0;
 
     __try {
-        RaiseException(MS_VC_EXCEPTION, 0, sizeof(info)/sizeof(DWORD), (ULONG_PTR*)&info);
+        RaiseException(MS_VC_EXCEPTION, 0, sizeof(info)/sizeof(ULONG_PTR), (ULONG_PTR*)&info);
     }
     __except(EXCEPTION_CONTINUE_EXECUTION) {
     }

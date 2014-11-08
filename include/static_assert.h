907cd3dd01098c5b7437173b5f800ecdf03946bb
Chris Robinson <chris.kcat@gmail.com>
2014-11-07 19:40:11 -0800

    Add a workaround for compilers without __COUNTER__
    This can make GCC pretty noisey, complaining "declaration does not declare
    anything" for each static_assert, but it should still function on such older
    compilers.

--- include/static_assert.h
+++ include/static_assert.h
@@ -7,10 +7,12 @@
 #ifndef static_assert
 #ifdef HAVE_C11_STATIC_ASSERT
 #define static_assert _Static_assert
-#else
+#elif defined(__COUNTER__)
 #define CTASTR2(_pre,_post) _pre##_post
 #define CTASTR(_pre,_post) CTASTR2(_pre,_post)
 #define static_assert(_cond, _msg) typedef struct { int CTASTR(static_assert_failed_at_line_,__LINE__) : !!(_cond); } CTASTR(static_assertion_,__COUNTER__)
+#else
+#define static_assert(_cond, _msg) struct { int CTASTR(static_assert_failed_at_line_,__LINE__) : !!(_cond); }
 #endif
 #endif
 

19f79be57b8e768f44710b6d26017bc1f8c8fbda
Chris Robinson <chris.kcat@gmail.com>
2015-03-02 23:05:25 -0800

    Fix static_assert when __COUNTER__ isn't available

--- include/static_assert.h
+++ include/static_assert.h
@@ -7,12 +7,14 @@
 #ifndef static_assert
 #ifdef HAVE_C11_STATIC_ASSERT
 #define static_assert _Static_assert
-#elif defined(__COUNTER__)
+#else
 #define CTASTR2(_pre,_post) _pre##_post
 #define CTASTR(_pre,_post) CTASTR2(_pre,_post)
+#if defined(__COUNTER__)
 #define static_assert(_cond, _msg) typedef struct { int CTASTR(static_assert_failed_at_line_,__LINE__) : !!(_cond); } CTASTR(static_assertion_,__COUNTER__)
 #else
 #define static_assert(_cond, _msg) struct { int CTASTR(static_assert_failed_at_line_,__LINE__) : !!(_cond); }
+#endif
 #endif
 #endif
 

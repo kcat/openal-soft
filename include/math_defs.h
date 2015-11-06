858230f4528ad2f418f97414e3a8be46865c367c
Chris Robinson <chris.kcat@gmail.com>
2015-11-06 02:38:34 -0800

    Include float.h if present before defining math stuff

--- include/math_defs.h
+++ include/math_defs.h
@@ -1,6 +1,10 @@
 #ifndef AL_MATH_DEFS_H
 #define AL_MATH_DEFS_H
 
+#ifdef HAVE_FLOAT_H
+#include <float.h>
+#endif
+
 #define F_PI    (3.14159265358979323846f)
 #define F_PI_2  (1.57079632679489661923f)
 #define F_TAU   (6.28318530717958647692f)

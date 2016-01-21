bb0ac26ce1bc8966d87e5db2d9a9675bd939b6af
Chris Robinson <chris.kcat@gmail.com>
2016-01-21 02:33:45 -0800

    Explicit check if pthread_setname_np accepts only one parameter

--- common/threads.c
+++ common/threads.c
@@ -497,10 +497,10 @@ extern inline void alcall_once(alonce_flag *once, void (*callback)(void));
 void althrd_setname(althrd_t thr, const char *name)
 {
 #if defined(HAVE_PTHREAD_SETNAME_NP)
-#if defined(__APPLE__)
+#if defined(PTHREAD_SETNAME_NP_ONE_PARAM)
     if(althrd_equal(thr, althrd_current()))
         pthread_setname_np(name);
-#elif defined(__GNUC__)
+#else
     pthread_setname_np(thr, name);
 #endif
 #elif defined(HAVE_PTHREAD_SET_NAME_NP)

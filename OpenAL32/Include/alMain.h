3ba03c5a29bcebb1866f36555957c3c293a99962
Chris Robinson <chris.kcat@gmail.com>
2017-01-26 18:05:34 -0800

    Also log to __android_log_print on Android

--- OpenAL32/Include/alMain.h
+++ OpenAL32/Include/alMain.h
@@ -914,6 +914,13 @@ void al_print(const char *type, const char *func, const char *fmt, ...) DECL_FOR
 #define AL_PRINT(T, ...) al_print((T), __FUNCTION__, __VA_ARGS__)
 #endif
 
+#ifdef __ANDROID__
+#include <android/log.h>
+#define LOG_ANDROID(T, MSG, ...) __android_log_print(T, "openal", "AL lib: %s: "MSG, __FUNCTION__ , ## __VA_ARGS__)
+#else
+#define LOG_ANDROID(T, MSG, ...) ((void)0)
+#endif
+
 enum LogLevel {
     NoLog,
     LogError,
@@ -926,21 +933,25 @@ extern enum LogLevel LogLevel;
 #define TRACEREF(...) do {                                                    \
     if(LogLevel >= LogRef)                                                    \
         AL_PRINT("(--)", __VA_ARGS__);                                        \
+    LOG_ANDROID(ANDROID_LOG_VERBOSE, __VA_ARGS__);                            \
 } while(0)
 
 #define TRACE(...) do {                                                       \
     if(LogLevel >= LogTrace)                                                  \
         AL_PRINT("(II)", __VA_ARGS__);                                        \
+    LOG_ANDROID(ANDROID_LOG_DEBUG, __VA_ARGS__);                              \
 } while(0)
 
 #define WARN(...) do {                                                        \
     if(LogLevel >= LogWarning)                                                \
         AL_PRINT("(WW)", __VA_ARGS__);                                        \
+    LOG_ANDROID(ANDROID_LOG_WARN, __VA_ARGS__);                               \
 } while(0)
 
 #define ERR(...) do {                                                         \
     if(LogLevel >= LogError)                                                  \
         AL_PRINT("(EE)", __VA_ARGS__);                                        \
+    LOG_ANDROID(ANDROID_LOG_ERROR, __VA_ARGS__);                              \
 } while(0)
 
 

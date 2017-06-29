a69d608a1ec38e6afd903224e86c0bf29b8d0623
Chris Robinson <chris.kcat@gmail.com>
2017-06-29 10:11:31 -0700

    Define a backup log2f if the compiler doesn't have it

--- Alc/ALc.c
+++ Alc/ALc.c
@@ -1213,9 +1213,6 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *jvm, void* UNUSED(reserved))
     pthread_setspecific(gJVMThreadKey, env);
     return JNI_VERSION_1_4;
 }
-
-/* Android doesn't have log2f, really? */
-#define log2f(x)  (logf(x) / logf(2.0f))
 #endif
 
 
--- common/math_defs.h
+++ common/math_defs.h
@@ -22,6 +22,13 @@ static const union msvc_inf_hack {
 #define HUGE_VALF (msvc_inf_union.f)
 #endif
 
+#ifndef HAVE_LOG2F
+static inline float log2f(float f)
+{
+    return logf(f) / logf(2.0f);
+}
+#endif
+
 #define DEG2RAD(x)  ((float)(x) * (F_PI/180.0f))
 #define RAD2DEG(x)  ((float)(x) * (180.0f/F_PI))
 

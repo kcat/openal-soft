051828344ecaeff0140034693e3f63355f490e9b
Chris Robinson <chris.kcat@gmail.com>
2017-08-20 01:39:24 -0700

    Improve unicode handling for makehrtf
    Command line parameters and filenames are now unicode-aware (the .def files
    should be UTF-8 encoded, if they contain any non-ASCII-7 characters). Unicode
    characters might not display correctly in the console, but it should process
    them correctly.

--- utils/makehrtf.c
+++ utils/makehrtf.c
@@ -60,6 +60,7 @@
 
 #include "config.h"
 
+#define _UNICODE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -81,6 +82,61 @@
 
 #ifndef HUGE_VAL
 #define HUGE_VAL                     (1.0 / 0.0)
+#endif
+
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+static char *ToUTF8(const wchar_t *from)
+{
+    char *out = NULL;
+    int len;
+    if((len=WideCharToMultiByte(CP_UTF8, 0, from, -1, NULL, 0, NULL, NULL)) > 0)
+    {
+        out = calloc(sizeof(*out), len);
+        WideCharToMultiByte(CP_UTF8, 0, from, -1, out, len, NULL, NULL);
+        out[len-1] = 0;
+    }
+    return out;
+}
+
+static WCHAR *FromUTF8(const char *str)
+{
+    WCHAR *out = NULL;
+    int len;
+
+    if((len=MultiByteToWideChar(CP_UTF8, 0, str, -1, NULL, 0)) > 0)
+    {
+        out = calloc(sizeof(WCHAR), len);
+        MultiByteToWideChar(CP_UTF8, 0, str, -1, out, len);
+        out[len-1] = 0;
+    }
+    return out;
+}
+
+
+static FILE *my_fopen(const char *fname, const char *mode)
+{
+    WCHAR *wname=NULL, *wmode=NULL;
+    FILE *file = NULL;
+
+    wname = FromUTF8(fname);
+    wmode = FromUTF8(mode);
+    if(!wname)
+        fprintf(stderr, "Failed to convert UTF-8 filename: \"%s\"\n", fname);
+    else if(!wmode)
+        fprintf(stderr, "Failed to convert UTF-8 mode: \"%s\"\n", mode);
+    else
+        file = _wfopen(wname, wmode);
+
+    free(wname);
+    free(wmode);
+
+    return file;
+}
+#define fopen my_fopen
 #endif
 
 // The epsilon used to maintain signal stability.
@@ -2872,8 +2928,34 @@ static void PrintHelp(const char *argv0, FILE *ofile)
     fprintf(ofile, "                 Use of '%%r' will be substituted with the data set sample rate.\n");
 }
 
+#ifdef _WIN32
+#define main my_main
+int main(int argc, const char *argv[]);
+
+static char **arglist;
+static void cleanup_arglist(void)
+{
+    int i;
+    for(i = 0;arglist[i];i++)
+        free(arglist[i]);
+    free(arglist);
+}
+
+int wmain(int argc, const wchar_t *wargv[])
+{
+    int i;
+
+    atexit(cleanup_arglist);
+    arglist = calloc(sizeof(*arglist), argc);
+    for(i = 0;i < argc;i++)
+        arglist[i] = ToUTF8(wargv[i]);
+
+    return main(argc, (const char**)arglist);
+}
+#endif
+
 // Standard command line dispatch.
-int main(const int argc, const char *argv[])
+int main(int argc, const char *argv[])
 {
     const char *inName = NULL, *outName = NULL;
     OutputFormatT outFormat;

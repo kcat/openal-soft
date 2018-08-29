4411a8db4351e41f9e908adde65118fa0b56f619
foxer <zhoutianzhu@bytedance.com>
2018-08-29 22:08:30 +0800

    ios compile

---
+++ build_ios/version.h
@@ -0,0 +1,8 @@
+/* Define to the library version */
+#define ALSOFT_VERSION "1.18.2"
+
+/* Define the branch being built */
+#define ALSOFT_GIT_BRANCH "master"
+
+/* Define the hash of the head commit */
+#define ALSOFT_GIT_COMMIT_HASH "24b646de"

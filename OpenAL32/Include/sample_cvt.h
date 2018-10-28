5148a73a7bc8cc0cad1e157930c936c75e40da2e
Chris Robinson <chris.kcat@gmail.com>
2018-10-27 18:56:11 -0700

    Add missing extern "C"

--- OpenAL32/Include/sample_cvt.h
+++ OpenAL32/Include/sample_cvt.h
@@ -4,6 +4,10 @@
 #include "AL/al.h"
 #include "alBuffer.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern const ALshort muLawDecompressionTable[256];
 extern const ALshort aLawDecompressionTable[256];
 
@@ -11,5 +15,9 @@ void Convert_ALshort_ALima4(ALshort *dst, const ALubyte *src, ALsizei numchans, 
                             ALsizei align);
 void Convert_ALshort_ALmsadpcm(ALshort *dst, const ALubyte *src, ALsizei numchans, ALsizei len,
                                ALsizei align);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif /* SAMPLE_CVT_H */

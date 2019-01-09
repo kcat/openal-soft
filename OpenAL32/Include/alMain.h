b49c45d3a462a16c72bd824cd40e3b37d6e31939
Chris Robinson <chris.kcat@gmail.com>
2019-01-09 00:31:57 -0800

    Add prefixes for ALCdevice and ALCcontext

--- OpenAL32/Include/alMain.h
+++ OpenAL32/Include/alMain.h
@@ -740,6 +740,7 @@ struct ALCdevice {
     ALsizei channelsFromFmt() const noexcept { return ChannelsFromDevFmt(FmtChans, mAmbiOrder); }
     ALsizei frameSizeFromFmt() const noexcept { return bytesFromFmt() * channelsFromFmt(); }
 
+    static constexpr inline const char *CurrentPrefix() noexcept { return "ALCdevice::"; }
     DEF_NEWDEL(ALCdevice)
 };
 

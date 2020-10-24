88b9eb1560f4971efc6d65aab5faecdf2f0a10e2
Chris Robinson <chris.kcat@gmail.com>
2020-10-23 20:23:40 -0700

    Silence a couple more MSVC warnings

--- examples/alrecord.c
+++ examples/alrecord.c
@@ -56,14 +56,19 @@ static float msvc_strtof(const char *str, char **end)
 
 static void fwrite16le(ALushort val, FILE *f)
 {
-    ALubyte data[2] = { (ALubyte)(val&0xff), (ALubyte)((val>>8)&0xff) };
+    ALubyte data[2];
+    data[0] = (ALubyte)(val&0xff);
+    data[1] = (ALubyte)(val>>8);
     fwrite(data, 1, 2, f);
 }
 
 static void fwrite32le(ALuint val, FILE *f)
 {
-    ALubyte data[4] = { (ALubyte)(val&0xff), (ALubyte)((val>>8)&0xff), (ALubyte)((val>>16)&0xff),
-        (ALubyte)((val>>24)&0xff) };
+    ALubyte data[4];
+    data[0] = (ALubyte)(val&0xff);
+    data[1] = (ALubyte)((val>>8)&0xff);
+    data[2] = (ALubyte)((val>>16)&0xff);
+    data[3] = (ALubyte)(val>>24);
     fwrite(data, 1, 4, f);
 }
 

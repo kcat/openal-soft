43ec687ee94fffb90a97841406c36ce08291bf80
Chris Robinson <chris.kcat@gmail.com>
2022-01-05 20:32:03 -0800

    Add a third-order ambisonic decoder for HRTF
    Using the 20-channel dodecahedron, seems to be good enough to handle 16-channel
    third-order ambisonics.

--- utils/alsoft-config/mainwindow.cpp
+++ utils/alsoft-config/mainwindow.cpp
@@ -137,6 +137,7 @@ static const struct NameValuePair {
 }, hrtfModeList[] = {
     { "1st Order Ambisonic", "ambi1" },
     { "2nd Order Ambisonic", "ambi2" },
+    { "3rd Order Ambisonic", "ambi3" },
     { "Default (Full)", "" },
     { "Full", "full" },
 
@@ -767,11 +768,9 @@ void MainWindow::loadConfig(const QString &fname)
 
     QString hrtfmode{settings.value("hrtf-mode").toString().trimmed()};
     ui->hrtfmodeSlider->setValue(2);
-    ui->hrtfmodeLabel->setText(hrtfModeList[2].name);
-    /* The "basic" mode name is no longer supported, and "ambi3" is temporarily
-     * disabled. Use "ambi2" instead.
-     */
-    if(hrtfmode == "basic" || hrtfmode == "ambi3")
+    ui->hrtfmodeLabel->setText(hrtfModeList[3].name);
+    /* The "basic" mode name is no longer supported. Use "ambi2" instead. */
+    if(hrtfmode == "basic")
         hrtfmode = "ambi2";
     for(int i = 0;hrtfModeList[i].name[0];i++)
     {

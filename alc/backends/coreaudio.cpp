357fb467fe7ffe5d4898ef4333dac9f679a79762
Chris Robinson <chris.kcat@gmail.com>
2022-02-23 02:26:25 -0800

    Define the CoreAudio default name only when needed

--- alc/backends/coreaudio.cpp
+++ alc/backends/coreaudio.cpp
@@ -51,8 +51,6 @@ namespace {
 #define CAN_ENUMERATE 1
 #endif
 
-static const char ca_device[] = "CoreAudio Default";
-
 
 #if CAN_ENUMERATE
 struct DeviceEntry {
@@ -246,6 +244,10 @@ void EnumerateDevices(std::vector<DeviceEntry> &list, bool isCapture)
     newdevs.shrink_to_fit();
     newdevs.swap(list);
 }
+
+#else
+
+static constexpr char ca_device[] = "CoreAudio Default";
 #endif
 
 

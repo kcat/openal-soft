6753601b1570876ffa6ac36f8a5fde10c35a9a7c
Chris Robinson <chris.kcat@gmail.com>
2022-12-18 20:25:01 -0800

    Avoid unreachable() in assume_aligned
    Another test to attempt to workaround MSVC build problems. Also, don't assume
    bit-wise value alignment for pointers as a default fallback.

--- common/opthelpers.h
+++ common/opthelpers.h
@@ -58,14 +58,16 @@ force_inline constexpr auto assume_aligned(T *ptr) noexcept
     return std::assume_aligned<alignment,T>(ptr);
 #elif HAS_BUILTIN(__builtin_assume_aligned)
     return static_cast<T*>(__builtin_assume_aligned(ptr, alignment));
+#elif defined(_MSC_VER)
+    constexpr std::size_t alignment_mask{(1<<alignment) - 1};
+    if((reinterpret_cast<std::uintptr_t>(ptr)&alignment_mask) == 0)
+        return ptr;
+    __assume(0);
 #elif defined(__ICC)
     __assume_aligned(ptr, alignment);
     return ptr;
 #else
-    constexpr std::size_t alignment_mask{(1<<alignment) - 1};
-    if((reinterpret_cast<std::uintptr_t>(ptr)&alignment_mask) == 0)
-        return ptr;
-    unreachable();
+    return ptr;
 #endif
 }
 

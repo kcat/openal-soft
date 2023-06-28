c014aabe07c3347330c0232d9d8cf6ffc8dacb1f
Deal(一线灵) <halx99@live.com>
2023-06-28 15:58:03 +0800

    [UWP] Also check openDevice to avoid crash

--- alc/backends/wasapi.cpp
+++ alc/backends/wasapi.cpp
@@ -577,7 +577,8 @@ struct DeviceHelper final : private IMMNotificationClient
         Platform::String^ devIfPath =
             devid.empty() ? (flow == eRender ? MediaDevice::GetDefaultAudioRenderId(deviceRole) : MediaDevice::GetDefaultAudioCaptureId(deviceRole))
             : ref new Platform::String(devid.data());
-
+        if (!devIfPath)
+            return E_POINTER;
         Concurrency::task<DeviceInformation^> createDeviceOp(
             DeviceInformation::CreateFromIdAsync(devIfPath, nullptr, DeviceInformationKind::DeviceInterface));
         auto status = createDeviceOp.then([&](DeviceInformation^ deviceInfo)

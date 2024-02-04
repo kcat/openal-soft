739bda14434ce3227ab8f75db9b9abd4ce643aef
Chris Robinson <chris.kcat@gmail.com>
2024-02-04 05:19:14 -0800

    Clarify ambiguity between size_t and DWORD

--- alc/backends/winmm.cpp
+++ alc/backends/winmm.cpp
@@ -509,7 +509,7 @@ void WinMMCapture::open(std::string_view name)
 
     // Allocate circular memory buffer for the captured audio
     // Make sure circular buffer is at least 100ms in size
-    const auto CapturedDataSize = std::max(size_t{mDevice->BufferSize},
+    const auto CapturedDataSize = std::max<size_t>(mDevice->BufferSize,
         BufferSize*mWaveBuffer.size());
 
     mRing = RingBuffer::Create(CapturedDataSize, mFormat.nBlockAlign, false);

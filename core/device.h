6b2df6aea340c41705cd6e9866549b6f468b0941
Chris Robinson <chris.kcat@gmail.com>
2024-02-24 07:44:54 -0800

    Calculate the ending mix counter value earlier

--- core/device.h
+++ core/device.h
@@ -300,26 +300,25 @@ struct DeviceBase {
     [[nodiscard]] auto frameSizeFromFmt() const noexcept -> uint { return bytesFromFmt() * channelsFromFmt(); }
 
     struct MixLock {
-        std::atomic<uint> &mCount;
-        const uint mLastVal;
+        DeviceBase *const self;
+        const uint mEndVal;
 
-        MixLock(std::atomic<uint> &count, const uint last_val) noexcept
-            : mCount{count}, mLastVal{last_val}
-        { }
-        /* Increment the mix count when the lock goes out of scope to "release"
-         * it (lsb should be 0).
+        MixLock(DeviceBase *device, const uint endval) noexcept : self{device}, mEndVal{endval} { }
+        MixLock(const MixLock&) = delete;
+        void operator=(const MixLock&) = delete;
+        /* Update the mix count when the lock goes out of scope to "release" it
+         * (lsb should be 0).
          */
-        ~MixLock() { mCount.store(mLastVal+2, std::memory_order_release); }
+        ~MixLock() { self->mMixCount.store(mEndVal, std::memory_order_release); }
     };
-    auto getWriteMixLock() noexcept
+    auto getWriteMixLock() noexcept -> MixLock
     {
         /* Increment the mix count at the start of mixing and writing clock
          * info (lsb should be 1).
          */
-        const auto mixCount = mMixCount.load(std::memory_order_relaxed);
-        mMixCount.store(mixCount+1, std::memory_order_relaxed);
-        std::atomic_thread_fence(std::memory_order_release);
-        return MixLock{mMixCount, mixCount};
+        auto mixCount = mMixCount.load(std::memory_order_relaxed);
+        mMixCount.store(++mixCount, std::memory_order_release);
+        return MixLock{this, ++mixCount};
     }
 
     /** Waits for the mixer to not be mixing or updating the clock. */

617de33db94063fe884cb457d742d19ffb6b681d
Chris Robinson <chris.kcat@gmail.com>
2024-04-16 04:50:18 -0700

    Fix an implicit array-to-pointer decay

--- common/alassert.h
+++ common/alassert.h
@@ -1,6 +1,8 @@
 #ifndef AL_ASSERT_H
 #define AL_ASSERT_H
 
+#include <array>
+
 #include "opthelpers.h"
 
 namespace al {
@@ -16,7 +18,7 @@ void do_assert(const char *message, int linenum, const char *filename, const cha
  */
 #define alassert(cond) do {                                                   \
     if(!(cond)) UNLIKELY                                                      \
-        al::do_assert("Assertion '" #cond "' failed", __LINE__, __FILE__, __func__); \
+        al::do_assert("Assertion '" #cond "' failed", __LINE__, __FILE__, std::data(__func__)); \
 } while(0)
 
 #endif /* AL_ASSERT_H */

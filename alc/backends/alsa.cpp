c05fc028d378801f4abe16eba212e033a388ca70
Chris Robinson <chris.kcat@gmail.com>
2024-05-01 21:42:06 -0700

    Handle a public private enum value in the ALSA backend

--- alc/backends/alsa.cpp
+++ alc/backends/alsa.cpp
@@ -452,6 +452,17 @@ int verify_state(snd_pcm_t *handle)
 
         case SND_PCM_STATE_DISCONNECTED:
             return -ENODEV;
+
+        /* ALSA headers have made this enum public, leaving us in a bind: use
+         * the enum despite being private and internal to the libasound, or
+         * ignore when an enum value isn't handled. We can't rely on it being
+         * declared either, since older headers don't have it and it could be
+         * removed in the future. We can't even really rely on its value, since
+         * being private/internal means it's subject to change, but this is the
+         * best we can do.
+         */
+        case 1024 /*SND_PCM_STATE_PRIVATE1*/:
+            assert(state != 1024);
     }
 
     return state;

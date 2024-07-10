71d3c811b5672f62d75c2756279812fceed58d74
Andrew Lamontagne <metallicafan212@gmail.com>
2024-07-09 20:13:47 -0700

    Redirect cubic to spline when loading alsoft.ini (#1018)
    * Redirect the old cubic ini option to spline, as that's the comparable type
    
    * Fix tabbing
    
    * Fix if statement spacing

--- core/voice.cpp
+++ core/voice.cpp
@@ -148,8 +148,13 @@ void Voice::InitMixer(std::optional<std::string> resopt)
         };
 
         std::string_view resampler{*resopt};
-        if(al::case_compare(resampler, "cubic"sv) == 0
-            || al::case_compare(resampler, "sinc4"sv) == 0
+		
+        if (al::case_compare(resampler, "cubic"sv) == 0)
+        {
+            WARN("Resampler option \"%s\" is deprecated, using spline\n", resopt->c_str());
+            resampler = "spline"sv;
+        }
+        else if(al::case_compare(resampler, "sinc4"sv) == 0
             || al::case_compare(resampler, "sinc8"sv) == 0)
         {
             WARN("Resampler option \"%s\" is deprecated, using gaussian\n", resopt->c_str());

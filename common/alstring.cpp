602c68789d5c52e30c3a276f089c741955704f1e
Chris Robinson <chris.kcat@gmail.com>
2025-04-11 03:17:32 -0700

    Use a separate function to do the compare loop

--- common/alstring.cpp
+++ common/alstring.cpp
@@ -5,9 +5,29 @@
 
 #include <algorithm>
 #include <cctype>
-#include <cwctype>
 #include <cstring>
+#include <cwctype>
+#include <iterator>
 
+
+namespace {
+
+template<std::input_iterator T, typename F> constexpr
+auto find_not_zero(T iter0, T end0, T iter1, T end1, F&& comp) -> int
+{
+    auto count = std::min(std::distance(iter0, end0), std::distance(iter1, end1));
+    while(count > 0)
+    {
+        if(const auto ret = comp(*iter0, *iter1))
+            return ret;
+        ++iter0;
+        ++iter1;
+        --count;
+    }
+    return 0;
+}
+
+} // namespace
 
 namespace al {
 
@@ -15,16 +35,15 @@ int case_compare(const std::string_view str0, const std::string_view str1) noexc
 {
     using Traits = std::string_view::traits_type;
 
-    auto ch0 = str0.cbegin();
-    auto ch1 = str1.cbegin();
-    auto ch1end = ch1 + std::min(str0.size(), str1.size());
-    while(ch1 != ch1end)
+    const auto diff = find_not_zero(str0.cbegin(), str0.cend(), str1.cbegin(), str1.cend(),
+        [](const char ch0, const char ch1) -> int
     {
-        const int u0{std::toupper(Traits::to_int_type(*ch0))};
-        const int u1{std::toupper(Traits::to_int_type(*ch1))};
-        if(const int diff{u0-u1}) return diff;
-        ++ch0; ++ch1;
-    }
+        const auto u0 = std::toupper(Traits::to_int_type(ch0));
+        const auto u1 = std::toupper(Traits::to_int_type(ch1));
+        return u0 - u1;
+    });
+    if(diff != 0)
+        return diff;
 
     if(str0.size() < str1.size()) return -1;
     if(str0.size() > str1.size()) return 1;
@@ -35,16 +54,15 @@ int case_compare(const std::wstring_view str0, const std::wstring_view str1) noe
 {
     using Traits = std::wstring_view::traits_type;
 
-    auto ch0 = str0.cbegin();
-    auto ch1 = str1.cbegin();
-    auto ch1end = ch1 + std::min(str0.size(), str1.size());
-    while(ch1 != ch1end)
+    const auto diff = find_not_zero(str0.cbegin(), str0.cend(), str1.cbegin(), str1.cend(),
+        [](const wchar_t ch0, const wchar_t ch1) -> int
     {
-        const auto u0 = std::towupper(Traits::to_int_type(*ch0));
-        const auto u1 = std::towupper(Traits::to_int_type(*ch1));
-        if(const auto diff = static_cast<int>(u0-u1)) return diff;
-        ++ch0; ++ch1;
-    }
+        const auto u0 = std::towupper(Traits::to_int_type(ch0));
+        const auto u1 = std::towupper(Traits::to_int_type(ch1));
+        return static_cast<int>(u0 - u1);
+    });
+    if(diff != 0)
+        return diff;
 
     if(str0.size() < str1.size()) return -1;
     if(str0.size() > str1.size()) return 1;

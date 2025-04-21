5dad52d6f1362fb85e72613ca374e7091ced99cf
Chris Robinson <chris.kcat@gmail.com>
2025-04-21 10:57:12 -0700

    Remove pre-C++20 fallback functions

--- common/alstring.h
+++ common/alstring.h
@@ -29,29 +29,17 @@ auto case_compare(const std::string_view str0, const std::string_view str1) noex
 auto case_compare(const std::wstring_view str0, const std::wstring_view str1) noexcept -> std::weak_ordering;
 
 /* C++20 changes path::u8string() to return a string using a new/distinct
- * char8_t type for UTF-8 strings. However, support for this with standard
- * string functions is totally inadequate, and we already hold UTF-8 with plain
- * char strings. So this function is used to reinterpret a char8_t string as a
- * char string_view.
+ * char8_t type for UTF-8 strings, and deprecates u8path in favor of using
+ * fs::path(char8_t*). However, support for this type with standard string
+ * functions is totally inadequate, and we already hold UTF-8 with plain char*
+ * strings. So these functions are used to reinterpret between char and char8_t
+ * string views.
  */
-#if defined(__cpp_lib_char8_t) && __cpp_lib_char8_t >= 201907L
-inline auto u8_as_char(const std::u8string_view str) -> std::string_view
-#else
-inline auto u8_as_char(const std::string_view str) -> std::string_view
-#endif
-{
-    return std::string_view{reinterpret_cast<const char*>(str.data()), str.size()};
-}
-
-#if defined(__cpp_lib_char8_t) && __cpp_lib_char8_t >= 201907L
 inline auto char_as_u8(const std::string_view str) -> std::u8string_view
-{
-    return std::u8string_view{reinterpret_cast<const char8_t*>(str.data()), str.size()};
-}
-#else
-inline auto char_as_u8(const std::string_view str) -> std::string_view
-{ return str; }
-#endif
+{ return std::u8string_view{reinterpret_cast<const char8_t*>(str.data()), str.size()}; }
+
+inline auto u8_as_char(const std::u8string_view str) -> std::string_view
+{ return std::string_view{reinterpret_cast<const char*>(str.data()), str.size()}; }
 
 } // namespace al
 

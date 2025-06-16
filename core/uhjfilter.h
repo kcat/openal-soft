4a9fb200276f34e85e5fb411ba10db28d0318cd2
Chris Robinson <chris.kcat@gmail.com>
2025-06-16 06:15:55 -0700

    Update the default super stereo width

--- core/uhjfilter.h
+++ core/uhjfilter.h
@@ -138,8 +138,14 @@ struct SIMDALIGN DecoderBase {
     /**
      * The width factor for Super Stereo processing. Can be changed in between
      * calls to decode, with valid values being between 0...0.7.
+     *
+     * 0.46 seens to produce the least amount of channel bleed when the output
+     * is subsequently UHJ encoded (given a stereo sound with a noise on the
+     * left buffer channel, for instance, when decoded with UhjStereoDecoder
+     * and then encoded with UhjEncoder, the right output channel was at its
+     * quietest).
      */
-    float mWidthControl{0.593f};
+    float mWidthControl{0.46f};
 };
 
 template<std::size_t N>
